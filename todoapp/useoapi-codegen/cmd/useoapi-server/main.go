e651be1efaa7f765715c76d19063a5b97e7cba52
podhmo <ababjam61+github@gmail.com>
2019-06-11 00:07:49 +0900

    fake implementation

--- todoapp/useoapi-codegen/cmd/useoapi-server/main.go
+++ todoapp/useoapi-codegen/cmd/useoapi-server/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	echo "github.com/labstack/echo/v4"
-	"github.com/podhmo-sandbox/apiserver-examples/todoapp/useoapi-codegen/cmd/useoapi-server/api"
+	api "github.com/podhmo-sandbox/apiserver-examples/todoapp/useoapi-codegen/Openapi3"
 )
 
 // API :
@@ -18,4 +18,5 @@ func main() {
 	e := echo.New()
 	var myapi API
 	api.RegisterHandlers(e, &myapi)
+	e.Logger.Fatal(e.Start(":8080"))
 }
